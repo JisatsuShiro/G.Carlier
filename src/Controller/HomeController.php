2e7b0c40f61adbafd8a298361fbebbd9d4219603
Shiro <guillaume.carlier@hotmail.com>
2022-01-13 11:38:44 +0100

    mise a jour des projets

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,4 +13,22 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig');
     }
+
+    #[Route('/eni-encheres', name: 'eni-encheres')]
+    public function encheres(): Response
+    {
+        return $this->render('projects/encheres.html.twig');
+    }
+
+    #[Route('/eni-sortir', name: 'eni-sortir')]
+    public function sortir(): Response
+    {
+        return $this->render('projects/sortir.html.twig');
+    }
+
+    #[Route('/uniwave', name: 'uniwave')]
+    public function encheres(): Response
+    {
+        return $this->render('projects/uniwave-3D.html.twig');
+    }
 }
